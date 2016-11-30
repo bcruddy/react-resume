f698f48dbf0f969614d90169be5f87316b6eaedc
Brian Ruddy <brian.ruddy@ibm.com>
2016-11-30 17:23:15 -0500

    include mobile meta tag

--- src/javascript/components/Head.js
+++ src/javascript/components/Head.js
@@ -4,10 +4,9 @@ export default class Head extends Component {
     render () {
         return (
             <head>
-                <title>React Resume</title>
-
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>static cv</title>
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
-
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
             </head>
         );
