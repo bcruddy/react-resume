6f0110675a9b5e4ec13c25baf96f71bf9808e769
Brian Ruddy <brian.ruddy@ibm.com>
2016-12-04 15:46:17 -0500

    move lib directory

--- src/lib/generate-static-html.js
+++ src/lib/generate-static-html.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import Root from '../components/Root.js';
+import Root from '../javascript/components/Root.js';
 import fs from 'fs';
 import path from 'path';
 
-import data from '../../../aboutData';
+import data from '../../aboutData';
 
 let content = `<!DOCTYPE html>${ReactDOMServer.renderToStaticMarkup(<Root data={data} />)}\n`,
-    root = path.join(__dirname, '..', '..', '..'),
+    root = path.join(__dirname, '..', '..'),
     outputDest = path.join(root, 'docs', 'index.html');
 
 fs.writeFileSync(outputDest, content);
