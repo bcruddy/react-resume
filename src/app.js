ad321e3f7733717b05a445b31bc189053ac03f16
Brian Ruddy <brian.ruddy@ibm.com>
2016-12-04 15:42:54 -0500

    set up body-parser

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import Root from './javascript/components/Root.js';
@@ -8,10 +10,12 @@ import data from '../aboutData';
 const app = express();
 
 app
+    .use(bodyParser.urlencoded({ extended: false }))
+    .use(bodyParser.json())
+    // routes
     .use('/', (req, res) => {
         res.send(`<!DOCTYPE html>${ReactDOMServer.renderToStaticMarkup(<Root data={data} />)}\n`);
     })
-
     .use('*', (req, res) => {
         res.status(301).redirect('/');
     });
